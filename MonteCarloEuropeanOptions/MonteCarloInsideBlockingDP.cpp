7154c779a70a5ba40808e0d85fa2c89365d9dd2d
root <root@localhost.localdomain>
2021-03-13 04:53:42 -0500

    reworked folders, with Cloudify

--- MonteCarloEuropeanOptions/MonteCarloInsideBlockingDP.cpp
+++ MonteCarloEuropeanOptions/MonteCarloInsideBlockingDP.cpp
@@ -23,9 +23,11 @@ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLI
 #include <mkl_vsl.h>
 #include <mkl_service.h>
 
-/******* VERSION *******/
+//Version 1.1: Modify inner loop, conditionally update reduction variables.
 #define MAJOR 1
-#define MINOR 2
+#define MINOR 1
+
+
 
 
 #define MAX_THREADS 288
@@ -75,21 +77,52 @@ static const double  RLog2E	= -RISKFREE * M_LOG2E;
 static const double MuLog2E	= M_LOG2E*(RISKFREE - 0.5 * VOLATILITY * VOLATILITY);
 static const double VLog2E	= M_LOG2E * VOLATILITY;
 
-// Black-Scholes Reference
-void BlackScholesRefImpl(
-    double& callResult,
-    double  Sf, //Stock price
-    double  Xf, //Option strike
-    double  Tf, //Option years
-    double  Rf, //Riskless rate
-    double  Vf  //Volatility rate
-)
+
+
+// CND Function Polynomial Approximation 
+
+static double CND(double d)
 {
-    // BSM Formula: https://www.nobelprize.org/prizes/economic-sciences/1997/press-release/
-	double S = Sf, L = Xf, t = Tf, r = Rf, sigma = Vf;
-	double N_d1 = 1. / 2. + 1. / 2. * erf(((log(S / L) + (r + 0.5 * sigma * sigma) * t) / (sigma * sqrt(t))) / sqrt(2.));
-	double N_d2 = 1. / 2. + 1. / 2. * erf(((log(S / L) + (r - 0.5 * sigma * sigma) * t) / (sigma * sqrt(t))) / sqrt(2.));
-	callResult = (S * N_d1 - L*exp(-r*t) * N_d2);
+    static const double A1       = 0.31938153;
+    static const double A2       = -0.356563782;
+    static const double A3       = 1.781477937;
+    static const double A4       = -1.821255978;
+    static const double A5       = 1.330274429;
+    static const double RSQRT2PI = 0.39894228040143267793994605993438;
+
+    const double K = 1.0 / (1.0 + 0.2316419 * fabs(d));
+
+    double cnd = RSQRT2PI * exp(- 0.5 * d * d) *
+    (K * (A1 + K * (A2 + K * (A3 + K * (A4 + K * A5)))));
+
+    if(d > 0)
+        cnd = 1.0 - cnd;
+
+    return cnd;
+}
+
+//BlackScholes Formula
+
+static void BlackScholesBodyCPU(double& callResult,
+                                double& putResult,
+                                double  Sf, //Stock price
+                                double  Xf, //Option strike
+                                double  Tf, //Option years
+                                double  Rf, //Riskless rate
+                                double  Vf) //Volatility rate
+{
+    const double S = Sf, X = Xf, T = Tf, R = Rf, V = Vf;
+
+    const double sqrtT = sqrt(T);
+    const double    d1 = (log(S / X) + (R + 0.5 * V * V) * T) / (V * sqrtT);
+    const double    d2 = d1 - V * sqrtT;
+    const double CNDD1 = CND(d1);
+    const double CNDD2 = CND(d2);
+
+    //Calculate Call and Put simultaneously
+    const double expRT = exp(- R * T);
+    callResult   = (S * CNDD1 - X * expRT * CNDD2);
+    putResult    = (X * expRT * (1.0 - CNDD2) - S * (1.0 - CNDD1));
 }
 
 static long long suffixed_atoll(const char *nptr)
@@ -177,7 +210,7 @@ int main(int argc, char* argv[])
        VERBOSE = suffixed_atoll(argv[5]);
 
 
-    printf("Double Precision Monte-Carlo European Option Pricing, version %d.%d\n\n", MAJOR, MINOR);
+    printf("Monte Carlo European Option Pricing Double Precision version %d.%d\n\n", MAJOR, MINOR);
     printf("Build Time       = %s %s\n", __DATE__, __TIME__);
     printf("Path Length      = %d\n", RAND_N);
     printf("Number of Options= %d\n", OPT_N);
@@ -191,121 +224,122 @@ int main(int argc, char* argv[])
 #else
     int NTHREADS = 1;
 #endif
+// Declare random number buffer and random number sequence descriptors
+// (should be dynamically allocated based on NTHREADS)
 
-    // calculate the block number based on block size
+// calculate the block number based on block size
     const int nblocks = RAND_N/RAND_BLOCK_LENGTH;
 
-    for(int ITER=0; ITER<NUM_ITERS ;ITER++)
-    {
-    
+for(int ITER=0; ITER<NUM_ITERS ;ITER++)
+{
+
 #pragma omp parallel reduction(+ : sum_delta) reduction(+ : sum_ref) reduction(+ : sumReserve) reduction(max : max_delta)
-        {
+    {
 #ifdef _OPENMP
-            int threadID = omp_get_thread_num();
+        int threadID = omp_get_thread_num();
 #else
-            int threadID = 0;
+        int threadID = 0;
 #endif
-            unsigned int randseed = RANDSEED + threadID;
-            srand(randseed);
-            double *CallResultList     = (double *)scalable_aligned_malloc(mem_size, SIMDALIGN);
-            double *CallConfidenceList = (double *)scalable_aligned_malloc(mem_size, SIMDALIGN);
-            double *StockPriceList     = (double *)scalable_aligned_malloc(mem_size, SIMDALIGN);
-            double *OptionStrikeList   = (double *)scalable_aligned_malloc(mem_size, SIMDALIGN);
-            double *OptionYearsList    = (double *)scalable_aligned_malloc(mem_size, SIMDALIGN);
-        
-            float *samples;
-            VSLStreamStatePtr rngStream;
+        unsigned int randseed = RANDSEED + threadID;
+        srand(randseed);
+        double *CallResultList     = (double *)scalable_aligned_malloc(mem_size, SIMDALIGN);
+        double *CallConfidenceList = (double *)scalable_aligned_malloc(mem_size, SIMDALIGN);
+        double *StockPriceList     = (double *)scalable_aligned_malloc(mem_size, SIMDALIGN);
+        double *OptionStrikeList   = (double *)scalable_aligned_malloc(mem_size, SIMDALIGN);
+        double *OptionYearsList    = (double *)scalable_aligned_malloc(mem_size, SIMDALIGN);
     
-            for(int i = 0; i < OPT_PER_THREAD; i++)
-            {
-                CallResultList[i]     = 0.0;
-                CallConfidenceList[i] = 0.0;
-                StockPriceList[i]     = RandFloat_T(5.0, 50.0, &randseed);
-                OptionStrikeList[i]   = RandFloat_T(10.0, 25.0, &randseed);
-                OptionYearsList[i]    = RandFloat_T(1.0, 5.0, &randseed);
-            }
-            samples = (float *)scalable_aligned_malloc(RAND_BLOCK_LENGTH * sizeof(float), SIMDALIGN);
-        
-            vslNewStream(&rngStream, VSL_BRNG_SFMT19937, RANDSEED + threadID); 
+        float *samples;
+        VSLStreamStatePtr rngStream;
+
+        for(int i = 0; i < OPT_PER_THREAD; i++)
+        {
+            CallResultList[i]     = 0.0;
+            CallConfidenceList[i] = 0.0;
+            StockPriceList[i]     = RandFloat_T(5.0, 50.0, &randseed);
+            OptionStrikeList[i]   = RandFloat_T(10.0, 25.0, &randseed);
+            OptionYearsList[i]    = RandFloat_T(1.0, 5.0, &randseed);
+        }
+        samples = (float *)scalable_aligned_malloc(RAND_BLOCK_LENGTH * sizeof(float), SIMDALIGN);
     
-    #pragma omp barrier
-            if (threadID == 0)
+        vslNewStream(&rngStream, VSL_BRNG_SFMT19937, RANDSEED + threadID); 
+
+#pragma omp barrier
+        if (threadID == 0)
+        {
+            sTime = second();
+        }
+
+        for(int opt = 0; opt < OPT_PER_THREAD; opt++)
+        {
+            const double VBySqrtT = VLog2E * sqrt(OptionYearsList[opt]);
+            const double sqrt_2_sigma = SQRT_2*VBySqrtT;
+            const double MuByT    = MuLog2E * OptionYearsList[opt];
+            const double Y        = StockPriceList[opt];
+            const double Z        = OptionStrikeList[opt];
+                
+            double v0 = 0.0;
+            double v1 = 0.0;
+            for(int block = 0; block < nblocks; ++block)
             {
-                sTime = second();
-            }
-    
-            for(int opt = 0; opt < OPT_PER_THREAD; opt++)
-            {
-                const double VBySqrtT = VLog2E * sqrt(OptionYearsList[opt]);
-                const double sqrt_2_sigma = SQRT_2*VBySqrtT;
-                const double MuByT    = MuLog2E * OptionYearsList[opt];
-                const double Y        = StockPriceList[opt];
-                const double Z        = OptionStrikeList[opt];
-                    
-                double v0 = 0.0;
-                double v1 = 0.0;
-                for(int block = 0; block < nblocks; ++block)
+                vsRngGaussian (VSL_RNG_METHOD_GAUSSIAN_ICDF, rngStream, RAND_BLOCK_LENGTH, samples, MuByT, VBySqrtT);
+
+
+#pragma omp simd reduction(+:v0) reduction(+:v1)
+#pragma unroll(4)
+                for(int i=0; i < RAND_BLOCK_LENGTH; i++) 
                 {
-                    vsRngGaussian (VSL_RNG_METHOD_GAUSSIAN_ICDF, rngStream, RAND_BLOCK_LENGTH, samples, MuByT, VBySqrtT);
+                    double rngVal = samples[i];
+                    double callValue  = Y * exp2(rngVal) - Z;
     
-    
-    #pragma omp simd reduction(+:v0) reduction(+:v1)
-    #pragma unroll(4)
-                    for(int i=0; i < RAND_BLOCK_LENGTH; i++) 
+                    if (callValue>0.0)
                     {
-                        double rngVal = samples[i];
-                        double callValue  = Y * exp2(rngVal) - Z;
-        
-                        if (callValue>0.0)
-                        {
-                           v0 += callValue;
-                           v1 += callValue * callValue;
-                        }
+                       v0 += callValue;
+                       v1 += callValue * callValue;
                     }
-                } //end of block
-                const double  exprt    = exp2(RLog2E*OptionYearsList[opt]);
-                CallResultList[opt]     = exprt * v0 * INV_RAND_N;
-                const double  stdDev   = sqrt((F_RAND_N * v1 - v0 * v0) * STDDEV_DENOM);
-                CallConfidenceList[opt] = (double)(exprt * stdDev * CONFIDENCE_DENOM);
-            } //end of opt 
-    
-    #pragma omp barrier
-            if (threadID == 0) {
-                eTime = second();
-            }
-    
-            if(VERBOSE){
-                double delta = 0.0, ref = 0.0, L1norm = 0.0;
-                int max_index = 0;
-                double max_local  = 0.0;
-                for(int i = 0; i < OPT_PER_THREAD; i++){
-                    double callReference = 0.0;
-                    BlackScholesRefImpl(callReference,
-                                        StockPriceList[i],
-                                        OptionStrikeList[i], 
-                                        OptionYearsList[i],  
-                                        RISKFREE, 
-                                        VOLATILITY );
-                    ref   = callReference;
-                    delta = fabs(callReference - CallResultList[i]);
-                    sum_delta += delta;
-                    sum_ref   += fabs(ref);
-                    if(delta > 1e-6)
-                       sumReserve += CallConfidenceList[i] / delta;
-                       max_local = delta>max_local? delta: max_local;
                 }
-                max_delta = max_local>max_delta? max_local: max_delta;
+            } //end of block
+            const double  exprt    = exp2(RLog2E*OptionYearsList[opt]);
+            CallResultList[opt]     = exprt * v0 * INV_RAND_N;
+            const double  stdDev   = sqrt((F_RAND_N * v1 - v0 * v0) * STDDEV_DENOM);
+            CallConfidenceList[opt] = (double)(exprt * stdDev * CONFIDENCE_DENOM);
+        } //end of opt 
+
+#pragma omp barrier
+        if (threadID == 0) {
+            eTime = second();
+        }
+
+        if(VERBOSE){
+            double delta = 0.0, ref = 0.0, L1norm = 0.0;
+            int max_index = 0;
+            double max_local  = 0.0;
+            for(int i = 0; i < OPT_PER_THREAD; i++){
+                double callReference, putReference;
+                BlackScholesBodyCPU(
+                    callReference,
+                    putReference,
+                    StockPriceList[i],
+                    OptionStrikeList[i], OptionYearsList[i],  RISKFREE, VOLATILITY );
+                ref   = callReference;
+                delta = fabs(callReference - CallResultList[i]);
+                sum_delta += delta;
+                sum_ref   += fabs(ref);
+                if(delta > 1e-6)
+                   sumReserve += CallConfidenceList[i] / delta;
+                   max_local = delta>max_local? delta: max_local;
             }
-            scalable_aligned_free(samples);
-            scalable_aligned_free(CallResultList);
-            scalable_aligned_free(CallConfidenceList);
-            scalable_aligned_free(StockPriceList);
-            scalable_aligned_free(OptionStrikeList);
-            scalable_aligned_free(OptionYearsList);
-        
-            vslDeleteStream(&rngStream);
-        }//end of parallel block
-    }//end of iter loop
+            max_delta = max_local>max_delta? max_local: max_delta;
+        }
+        scalable_aligned_free(samples);
+        scalable_aligned_free(CallResultList);
+        scalable_aligned_free(CallConfidenceList);
+        scalable_aligned_free(StockPriceList);
+        scalable_aligned_free(OptionStrikeList);
+        scalable_aligned_free(OptionYearsList);
+    
+        vslDeleteStream(&rngStream);
+    }//end of parallel block
+}//end of iter loop
     
     if(VERBOSE){
         sumReserve          /= (double)OPT_N;
@@ -315,7 +349,6 @@ int main(int argc, char* argv[])
         printf("Max Error        = %E\n", max_delta);
         printf(sumReserve > 1.0f ? "Test passed\n" : "Test failed!\n");
     }
-    
     printf("==========================================\n");
     printf("Time Elapsed = %lf\n", eTime-sTime);
     printf("Opt/sec      = %lf\n", OPT_N/(eTime-sTime));
